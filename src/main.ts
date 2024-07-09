640cfb51b151765b6b6be3d95ed1d0f19f4efa4b
oc-garcia <octaviogarcia1988@gmail.com>
2024-07-09 10:51:44 -0300

    chore: Update dependencies and add @nestjs/swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  const config = new DocumentBuilder()
+    .setTitle('pettech-stock')
+    .setDescription('The pettech-stock Api Description')
+    .setVersion('1.0')
+    .addTag('stock')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(Number(process.env.PORT) || 3010);
 }
 bootstrap();
--- src/stock/controllers/stock.controller.ts
+++ src/stock/controllers/stock.controller.ts
@@ -16,6 +16,7 @@ import { z } from 'zod';
 import { ZodValidationPipe } from 'src/shared/pipe/zod-validation.pipe';
 // import { AuthGuard } from 'src/shared/guards/auth.guard';
 import { LoggingInterceptor } from 'src/shared/interceptors/logging.interceptor';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 const createStockSchema = z.object({
   name: z.string(),
@@ -29,7 +30,8 @@ const updateStockSchema = z.object({
 
 type CreateStocK = z.infer<typeof createStockSchema>;
 type UpdateStock = z.infer<typeof updateStockSchema>;
-
+@ApiBearerAuth()
+@ApiTags('stock')
 @UseInterceptors(LoggingInterceptor)
 @Controller('stock')
 export class StockController {
